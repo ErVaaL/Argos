1cf1790eca2b351933fa0335b0dc3f981581bd6e
ErVaaL <ErVaLofficial@protonmail.com>
2025-11-23 23:27:27 +0100

    Removed things that shouldn't be on repo

--- run-mongo.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/env bash
-docker run -d -p 27017:27017 --name argos-mongo -e MONGO_INITDB_DATABASE=argos mongo:8.0
--- settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-rootProject.name = "argos"
-
-
-include("application:argos-core")
-include("application:argos-application")
-include("application:adapters:argos-mongo-adapter")
-include("application:adapters:argos-m2m-security")
-include("public-services:argos-api")
