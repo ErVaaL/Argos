f255c801682b217c1c9cb02df0bf7c5de9156d57
ErVaaL <ErVaLofficial@protonmail.com>
2025-12-27 20:04:56 +0100

    Added process-service

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,14 @@
 rootProject.name = "argos-v2"
 
 include(
+    ":contracts",
     "services:resource-service:resource-core",
     "services:resource-service:resource-application",
     "services:resource-service:resource-adapters:mongo",
-    "services:resource-service:resource-bootstrap"
+    "services:resource-service:resource-bootstrap",
+    "services:process-service:process-core",
+    "services:process-service:process-application",
+    "services:process-service:process-adapters:grpc",
+    "services:process-service:process-adapters:mongo",
+    "services:process-service:process-bootstrap",
 )
